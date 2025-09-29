d9e776d56237c2865438ec2e102cdeed142f770e
shafigullina_aygul <sh.aygul@gmail.com>
2025-09-29 19:51:50 +0300

    homework_8

---
+++ lesson_8/homework_8_1_iter.py
@@ -0,0 +1,27 @@
+"""1. Пройти по списку с помощью итератора
+Создайте итератор для списка и выведите все его элементы с использованием next()."""
+cities = ['Москва', 'Санкт-Петербург', 'Нижний Новгород', 'Уфа', 'Екатеринбург', 'Челябинск', 'Иркутск']
+it_cities = iter(cities)
+print(next(it_cities))
+print(next(it_cities))
+print(next(it_cities))
+print(next(it_cities))
+print(next(it_cities))
+print(next(it_cities))
+print(next(it_cities))
+
+
+"""2. Итератор для строки
+Используйте iter() и next() для посимвольного вывода строки."""
+string = 'Automation'
+it_str = iter(string)
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
+print(next(it_str))
