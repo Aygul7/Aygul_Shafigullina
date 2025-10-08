2d24bf36cedb5faea4131306dad66af8e3bcf455
shafigullina_aygul <sh.aygul@gmail.com>
2025-10-08 21:03:39 +0300

    homework_14

---
+++ lesson_14/main.py
@@ -0,0 +1,8 @@
+from utils import greet
+from lesson_14.math_operations.addition import add
+from lesson_14.math_operations.subtraction import subtract
+
+greet("Алексей")  # Привет, Алексей!
+print(add(45, 23))
+print(subtract(54, 87))
+
